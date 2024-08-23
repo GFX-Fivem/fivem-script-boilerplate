9917aa2d5c8514ad9f4851d036670a666a6ca805
atiysu <107073209+atiysuu@users.noreply.github.com>
2024-08-23 20:00:25 +0300

    table_size, ClearTableKeys

--- client/utils.lua
+++ client/utils.lua
@@ -200,3 +200,21 @@ function PlayAnimationWithEntity(entity, dict, anim, cb)
     cb()
   end
 end
+
+function table_copy(t)
+    local t2 = {}
+    for k,v in pairs(t) do
+        t2[k] = v
+    end
+    return t2
+end
+
+function ClearTableKeys(t)
+    local t2 = {}
+
+    for k, v in pairs(t) do
+        table.insert(t2, v)
+    end
+
+    return t
+end
