8b7960edb585be838ec44030c44fa194c3159211
atiysu <107073209+atiysuu@users.noreply.github.com>
2024-09-14 14:54:58 +0300

    Added Discord

--- config/server_config.lua
+++ config/server_config.lua
@@ -1,4 +1,5 @@
 Config = {
     PhotoType = "steam",
     NoImage = "https://cdn.discordapp.com/attachments/736562375062192199/995301291976831026/noimage.png",
+    DiscordBotToken = "YOUR_DISCORD_BOT_TOKEN",
 }
