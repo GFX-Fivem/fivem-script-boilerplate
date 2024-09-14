709b46f2ff33f5f6f7d25695e06c538b49bcb56d
atiysu <107073209+atiysuu@users.noreply.github.com>
2024-09-14 16:13:34 +0300

    Fixed table_copy

--- client/utils.lua
+++ client/utils.lua
@@ -200,12 +200,14 @@ function PlayAnimationWithEntity(entity, dict, anim, cb)
   end
 end
 
-function table_copy(t)
-    local t2 = {}
-    for k,v in pairs(t) do
-        t2[k] = v
-    end
-    return t2
+function table_copy(obj, seen)
+    if type(obj) ~= 'table' then return obj end
+    if seen and seen[obj] then return seen[obj] end
+    local s = seen or {}
+    local res = setmetatable({}, getmetatable(obj))
+    s[obj] = res
+    for k, v in pairs(obj) do res[CopyTable(k, s)] = CopyTable(v, s) end
+    return res
 end
 
 function ClearTableKeys(t)
